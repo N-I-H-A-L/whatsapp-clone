bc79c43781d434164a37b706c8c090f958162037
N-I-H-A-L <notesnihalengg@gmail.com>
2023-06-24 21:24:40 +0530

    Clearing 'src' Folder

---
+++ firebase.js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBiB4yga21SnAsJOfaJuB_cfcxmudwQRDc",
+  authDomain: "whatsapp-clone-742ef.firebaseapp.com",
+  projectId: "whatsapp-clone-742ef",
+  storageBucket: "whatsapp-clone-742ef.appspot.com",
+  messagingSenderId: "1007285682469",
+  appId: "1:1007285682469:web:1cdc636efc2d3236dfdacc",
+  measurementId: "G-75FZHLED8V"
+};
+
